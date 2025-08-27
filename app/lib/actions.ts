efdf86658bcf2190c68f0ebbe1f60dca8adcebf9
Kevin Supa <kevinbsupa@gmail.com>
2025-08-27 15:11:38 -0600

    implemented try catch to actions and basica error handling pages

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -28,38 +28,52 @@ export async function createInvoice(formData: FormData) {
 
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0]
+    
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
-
-    revalidatePath('/dashboard/invoices')
-    redirect('/dashboard/invoices')
+        revalidatePath('/dashboard/invoices')
+        redirect('/dashboard/invoices')
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-    const {customerId, amount, status} = UpdateInvoice.parse({
+        const {customerId, amount, status} = UpdateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status')
     })
 
     const amountInCents = amount * 100;
+    
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
 
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-    `;
+        revalidatePath('/dashboard/invoices')
+        redirect('/dashboard/invoices')
+    } catch (e) {
+        console.error(e);
+    }
 
-    revalidatePath('/dashboard/invoices')
-    redirect('/dashboard/invoices')
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`
+    try {
+        await sql`
         DELETE from invoices WHERE id = ${id}
     `
-    revalidatePath('/dashboard/invoices');
+        revalidatePath('/dashboard/invoices');
+    } catch (e) {
+        console.log(`An error has occured: ${e}`);
+    }
+    
 }
