63bff534357b7d614f4483b21fe2ab230004c6d4
ksupa <kevinbsupa@gmail.com>
2025-08-28 14:13:59 -0600

    implemented the login page and authorization

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,8 @@ import { z } from 'zod'
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
-import { id } from 'zod/v4/locales';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -75,5 +76,23 @@ export async function deleteInvoice(id: string) {
     } catch (e) {
         console.log(`An error has occured: ${e}`);
     }
-    
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if(error instanceof AuthError) {
+            switch(error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
